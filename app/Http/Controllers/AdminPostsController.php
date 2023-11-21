dd8c17a820b9e90a653985bfda1aebc22a341204
3B032104 <3B032104@gm.student.ncut.edu.tw>
2023-11-22 00:35:35 +0800

    更新文章資料

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -9,7 +9,6 @@ class AdminPostsController extends Controller
 {
     public function index()
     {
-        //return view('admin.posts.index');
         $posts = Post::orderBy('created_at','DESC')->get();
         $data = ['posts' => $posts];
         return view('admin.posts.index',$data);
@@ -26,14 +25,18 @@ class AdminPostsController extends Controller
         return redirect()->route('admin.posts.index');
     }
 
-    public function edit($id)
+    public function edit(Post $post)
     {
-        return view('admin.posts.edit');
+        $data = [
+            'post'=> $post,
+        ];
+        return view('admin.posts.edit',$data);
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        $post->update($request->all());
+        return redirect()->route('admin.posts.index');
     }
 
     public function destroy($id)
